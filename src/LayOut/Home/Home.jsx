fa08a3eb69c854e07b95ea57a7c2903a6efd76a4
Foysal79 <onthor312001@gmail.com>
2023-12-21 11:56:20 +0600

    Banner page add title Sub Title and Explor Session Add

--- src/LayOut/Home/Home.jsx
+++ src/LayOut/Home/Home.jsx
@@ -3,7 +3,17 @@ import './Banner.css'
 
 const Home = () => {
     return (
-        <div className="background1" >
+        <div className="background1 font-Inter " >
+
+            <div className='space-y-2 md:space-y-3 w-9/12 mx-auto md:mx-0 md:w-full  lg:w-5/12 pt-16  md:pt-20 md:pl-10 lg:pt-36 lg:pl-20 '  >
+            <h2 className=' text-center md:text-start  font-bold text-3xl md:text-5xl text-white' >Manage work <br /> <span className='text-[#F2F7A1]' > Efficiently
+                </span> </h2>
+            <h4 className=' text-lg md:text-2xl font-semibold md:font-bold text-white' >Plan, Track and Organise your work.</h4>
+            <p className='text-sm text-justify md:text-start font-light md:font-medium text-white' >An Intranet platform to Manage projects, organise work and focus on what’s important.</p>
+            <div className='flex justify-center md:justify-start ' >
+            <button className='mt-10 md:mt-0 btn text-lg font-bold bg-[#F2F7A1] hover:bg-[#F2F7A1] text-black' >Let’s Explore</button>
+            </div>
+            </div>
             
         </div>
     );
--- src/LayOut/NavBar/NavBar.jsx
+++ src/LayOut/NavBar/NavBar.jsx
@@ -9,7 +9,7 @@ const NavBar = () => {
     </>
     return (
         <div>
-            <div className="navbar bg-base-100 md:px-12 lg:px-16 py-4 font-Inter">
+            <div className="navbar text-white md:px-12 lg:px-16 py-4 font-Inter bg-[#453C67] ">
   <div className="navbar-start">
     <div className="dropdown">
       <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
